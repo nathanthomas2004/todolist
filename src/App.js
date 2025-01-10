fe51e84e011caf93e5b8a433e11a164f1fa8a901
NT <nathaniel.thomas.2004@gmail.com>
2025-01-10 15:21:34 -0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ListItem from './components/ListItem';
+import ListItem from "./components/ListItem";
 
 function App() {
   return (
--- src/components/ListItem.js
+++
@@ -1,7 +0,0 @@
-export default function ListItem() {
-    return (
-        <div>
-            <h2>This is a list item</h2>
-        </div>
-    );
-}
